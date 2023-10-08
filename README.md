5f92876bfe7b5061ae48289014f006d529d779d6
Gopal Kataria <gopalkataria@outlook.com>
2023-10-08 14:13:53 +0530

    initial release

---
+++ README.md
@@ -0,0 +1,37 @@
+# Brainfuck Interpreter (C)
+
+This is a simple Brainfuck interpreter written in C.
+
+## Features
+This interpreter supports the following Brainfuck commands:
+
+* \+ : Increment the value at the current memory cell.
+* \- : Decrement the value at the current memory cell.
+* . : Output the value at the current memory cell as an ASCII character.
+* , : Read a single ASCII character from the user and store it in the current memory cell.
+* < : Move the data pointer to the left (decrement).
+* \> : Move the data pointer to the right (increment).
+* [ : If the value at the current memory cell is zero, jump to the corresponding ] command.
+* ] : If the value at the current memory cell is nonzero, jump to the corresponding [ command.
+
+## Compilation
+clone this repo and run 
+
+```bash
+gcc -Wall brainf\*\*k.c -o bf
+```
+this will generate an executable named bf. 
+
+## Usage 
+run a brainf**k file by passing it as an arument for bf executable
+```bash
+./bf test.bf
+```
+## Limitations
+This interpreter has the following limitations:
+
+* It only supports Brainfuck code with a maximum of 30,000 memory cells.
+* It does not support multi-byte characters or Unicode.
+* It does not support input or output redirection.
+## License
+This interpreter is licensed under the [MIT License](https://opensource.org/licenses/MIT). Feel free to use, modify, and distribute it as you see fit.
---
+++ brainf**k.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char CODE[10001], CELLS[30000] = {0};
+int ptr = 0;
+int validatechar(char c)
+{
+    char valid[] = "+-.,<>[]";
+    for (int i = 0; i < 8; i++)
+        if (c == valid[i])
+            return 1;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Error opening file");
+        return 2;
+    }
+
+    CODE[0] = '\0';
+    int c, opening_brack = 0, closing_brack = 0;
+    int char_count = 0;
+    while ((c = fgetc(file)) != EOF && char_count < 10000)
+    {
+        if (validatechar((char)c))
+        {
+            CODE[char_count] = (char)c;
+            opening_brack += ((char)c) == '[';
+            closing_brack += ((char)c) == ']';
+
+            char_count++;
+        }
+    }
+    if (opening_brack != closing_brack)
+    {
+        printf("All brackets dont match\n");
+        return -1;
+    }
+
+    CODE[char_count] = '\0';
+    fclose(file);
+
+    char curr;
+    int count = 0;
+    int codelen = strlen(CODE);
+    for (int i = 0; i < codelen; i++)
+    {
+        if (ptr < 0 || ptr > 30000)
+        {
+            // throw an error
+            printf("Pointer out of bounds\n");
+            return -1;
+        }
+
+        curr = CODE[i];
+
+        switch (curr)
+        {
+        case '>':
+            ptr++;
+            break;
+        case '<':
+            ptr--;
+            break;
+        case '+':
+            CELLS[ptr]++;
+            break;
+        case '-':
+            CELLS[ptr]--;
+            break;
+        case ',':
+            CELLS[ptr] = getchar();
+            break;
+
+        case '.':
+            printf("%c", CELLS[ptr]);
+            break;
+
+        case '[':
+            count = 0;
+            if (CELLS[ptr] == 0)
+            {
+                do
+                {
+                    count += CODE[i] == '[';
+                    count -= CODE[i] == ']';
+                    i++;
+                } while (count != 0);
+                i--; // to compensate for the i++ in the for loop
+            }
+            break;
+        case ']':
+            count = 0;
+            if (CELLS[ptr] != 0)
+            {
+                do
+                {
+                    count += CODE[i] == ']';
+                    count -= CODE[i] == '[';
+                    i--;
+                } while (count != 0);
+                i++; // to compensate for the i-- in the for loop
+            }
+            break;
+
+        default:
+            break;
+        }
+        // printf("%c", curr);
+    }
+
+    return 0;
+}
